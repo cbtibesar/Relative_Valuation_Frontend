01301b28151d21670490f1bcfc038220bbf19ff6
Conrad Tibesar <conradtibesar@conrads-mbp-2.truman.edu>
2022-01-27 12:50:09 -0600

    reset user-context branch

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import axiosInstance from '../services/authHeader';
 import { useNavigate } from 'react-router-dom';
 import AuthService from '../services/auth.service';
 
